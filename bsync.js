7f79cd306a9c2067713839bef134fac7433ef48b
artificial-life <proteus.protocol@gmail.com>
2016-06-10 22:17:39 +0600

    upd

--- bsync.js
+++ bsync.js
@@ -1,7 +1,8 @@
 var bs = require('browser-sync').create();
 
 bs.init({
-	proxy: "http://localhost:8080/components/iris-polymer-report-hub/demo/",
+	server: ["./", "./bower_components"],
 	files: ['*.html', 'demo/index.html'],
+	open: 'demo/index.html',
 	reloadDelay: 1000
 });
